c83733715166ed37f55da6402837af5a678ec1cb
emeric <emeric-lague@hotmail.com>
2021-02-21 14:22:28 -0500

    test-3

--- packages/regression_model/tests/test_predict.py
+++ packages/regression_model/tests/test_predict.py
@@ -4,8 +4,8 @@ import pathlib
 print(pathlib.Path().absolute())
 
 
-from regression_model.predict import make_prediction
-from regression_model.processing.data_management import load_dataset
+from regression_model.regression_model.predict import make_prediction
+from regression_model.regression_model.processing.data_management import load_dataset
 
 
 def test_make_single_prediction():
